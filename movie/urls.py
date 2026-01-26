3174e5634fa859eacab79af8bfc6c24ffc6c91a9
JohnH22 <93883042+JohnH22@users.noreply.github.com>
2026-01-26 03:02:27 +0200

    Changed and re-constructed sidebar, movie cards. Change Icons on sidebar and movie_list layouts also and added CSS on them. Refactor and created new classes to be able to use them as CSS selectors.

--- movie/urls.py
+++ movie/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.movie_list, name='movie_list'),
     path('movie/<int:pk>/', views.movie_detail, name='movie_detail'),
-    path('about_us', views.about_us, name='about_us'),
+    path('about_us/', views.about_us, name='about_us'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- movie/views.py
+++ movie/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Movie
+from .models import Movie, Category, Director
 from django.db.models import Q
 
 def movie_list(request):
-    cat = request.GET.get('cat')
-    txt = request.GET.get('txt')
-    if not cat:
-        if txt:
-            movies = Movie.objects.filter((Q(text__icontains=txt) | Q(title__icontains=txt)) & Q(published_date__lte=timezone.now())).order_by('published_date')
-        else:
-            movies = Movie.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    elif cat.isdigit():
-        cat=int(cat)
-        movies = Movie.objects.filter(published_date__lte=timezone.now()).filter(category=cat).order_by('published_date')
-    else:
-        movies = Movie.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'movie/movie_list.html', {'movies': movies})
+    cat = request.GET.get('cat', '')
+    txt = request.GET.get('txt', '')
+    dir = request.GET.get('dir', '')
+
+    movies = Movie.objects.all().order_by('-published_date')
+
+
+
+    if txt:
+            movies = movies.filter(Q(text__icontains=txt) | Q(title__icontains=txt))
+
+    if cat and cat.isdigit():
+            movies = movies.filter(category_id=int(cat))
+
+    if dir and dir.isdigit():
+            movies = movies.filter(director_id=int(dir))
+
+    return render(request, 'movie/movie_list.html', {'movies': movies, 'categories': Category.objects.all(), 'directors': Director.objects.all()})
 
 def movie_detail(request, pk):
     movie = get_object_or_404(Movie, pk=pk)
